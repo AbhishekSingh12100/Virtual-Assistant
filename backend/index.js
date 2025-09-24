07358f0de9d2c2ad433a4a1da66fd865e78417e0
Abhishek Singh <abhisheksingh12208799@gmail.com>
2025-09-24 22:23:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import userRouter from "./routes/user.routes.js";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtual-assistant-w8gt.onrender.com",
     credentials: true
 }));
 
